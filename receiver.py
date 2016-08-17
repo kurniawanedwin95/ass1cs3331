e9f01035361e15f9a0c46a42200686c651f08c41
Edwin Kurniawan <kurniawanedwin95@gmail.com>
2016-08-18 01:07:53 +1000

    sender updated with sys.args, receiver proposed socket and socketserver method

--- receiver.py
+++ receiver.py
@@ -1,11 +1,32 @@
 #!usr/bin/python
+#As I don't know whether SocketServer is able to do the assignment or not,
+#currently both options will be served.
 
-#will implement in socketserver
+#socketserver implementation
+import SocketServer
+import sys
+
+class UDPHandler(SocketServer.BaseRequestHandler):
+
+def handle(self):
+    data = self.request[0].strip()
+    s = self.request[1]
+    print '{} wrote:'.format(self.client_address[0])
+    print data
+    s.sendto(data.upper(), self.client_address)
+
+if __name__ == "__main__":
+    receiver_host_ip = #something
+    receiver_port = sys.arg[1]
+    server = SocketServer.UDPServer((receiver_host_ip, receiver_port))
+    server.serve_forever()
+
+#socket implementation
 import socket
-
+import sys
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = #something
-port = 20000 #somewhere unused and safe
+receiver_port = sys.arg[1]
 
 s.bind((host, port))
 s.listen(5) #why is this 5?
--- sender.py
+++ sender.py
@@ -1,5 +1,22 @@
 #!usr/bin/python
 
 import socket
+import sys
 
-#will get arguments
+#might comment them first, then add as more are implemented
+receiver_host_ip = sys.arg[1]
+receiver_port = sys.arg[2]
+# f = sys.arg[3]
+# MWS = sys.arg[4]
+# MSS = sys.arg[5]
+# timeout = sys.arg[6]
+# pdrop = sys.arg[7]
+# seed = sys.arg[8]
+
+s = socket.socket(AF_INET, SOCK_DGRAM)
+try:
+    s.connect((receiver_host_ip, receiver_port))
+    s.recv(1024)
+    #handshakes
+except socket.error:
+    print 'Could not connect to server, terminating program.\n'
