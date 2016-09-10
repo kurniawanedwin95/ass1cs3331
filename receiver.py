6650ed73684d659830cad212777cf9b1d745c071
kurniawanedwin95 <kurniawanedwin95@gmail.com>
2016-09-10 15:39:43 +1000

    socketserver not allowed, will make from socket

--- receiver.py
+++ receiver.py
@@ -1,25 +1,4 @@
 #!usr/bin/python
-#As I don't know whether SocketServer is able to do the assignment or not,
-#currently both options will be served.
-
-#socketserver implementation
-import SocketServer
-import sys
-
-class UDPHandler(SocketServer.BaseRequestHandler):
-
-def handle(self):
-    data = self.request[0].strip()
-    s = self.request[1]
-    print '{} wrote:'.format(self.client_address[0])
-    print data
-    s.sendto(data.upper(), self.client_address)
-
-if __name__ == "__main__":
-    receiver_host_ip = #something
-    receiver_port = sys.arg[1]
-    server = SocketServer.UDPServer((receiver_host_ip, receiver_port))
-    server.serve_forever()
 
 #socket implementation
 import socket
