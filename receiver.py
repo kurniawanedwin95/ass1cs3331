939b213c48cb07590334d41b42d01bb472146908
kurniawanedwin95 <kurniawanedwin95@gmail.com>
2016-09-12 00:59:43 +1000

    3 way handshake done, next will implement data transfer with header

--- receiver.py
+++ receiver.py
@@ -2,14 +2,33 @@
 
 #socket implementation
 import socket
+import struct
 import sys
+
+
+head = struct.Struct('? ? I I')
+
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = '' #local
-receiver_port = sys.arg[1]
+receiver_port = int(sys.argv[1])
 
-s.bind((host, port))
+s.bind((host, receiver_port))
 print 'server is waiting for UDP connection'
 while 1:
-  message, client = s.recvfrom(2048)
-  ACK = #something
-  s.sendto(ACK, client)
+  message, client = s.recvfrom(1024) #buffer size 1kb
+  #connection received
+  header = head.unpack(message)
+  print header
+  #might need an if statement here
+  if(header[0] == True and header[1] == False):
+    SYN = True
+    ACK = True
+    seq_num = header[2]+1
+    ack_num = 1
+    header = head.pack(SYN, ACK, seq_num, ack_num)
+    s.sendto(header, client)
+    print 'SYN+ACK packet sent'
+  message, client = s.recvfrom(1024)
+  header = head.unpack(message)
+  if(header[0] == False and header[1] == True):
+    print 'ACK Received'
--- sender.py
+++ sender.py
@@ -1,24 +1,48 @@
 #!usr/bin/python
 
 import socket
+import struct
 import sys
+import time
 
+#Main Function
 #might comment them first, then add as more are implemented
-receiver_host_ip = sys.arg[1]
-receiver_port = sys.arg[2]
+receiver_host_ip = sys.argv[1]
+receiver_port = int(sys.argv[2])
 # f = sys.arg[3]
-# MWS = sys.arg[4]
-# MSS = sys.arg[5]
-# timeout = sys.arg[6]
-# pdrop = sys.arg[7]
-# seed = sys.arg[8]
+# MSS = sys.arg[4]
+# timeout = sys.arg[5]
+# pdrop = sys.arg[6]
+# seed = sys.arg[7]
+head = struct.Struct('? ? I I')
 
-s = socket.socket(AF_INET, SOCK_DGRAM)
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
-    message = #something
-    s.sendto(message,(receiver_host_ip, receiver_port)) #sends SYN, can we do seq no?
-    message, client = s.recvfrom(2048) #reads reply, should receive SYN+ACK, buffer size is 2048
-    s.sendto('',(receiver_host_ip, receiver_port)) #sends ACK
-    #handshakes
+  #first we will simulate a TCP header and make it available in all our UDP packets
+  SYN = True
+  ACK = False
+  seq_num = 1#some randomly generated value
+  ack_num = 0
+  message = 'testin dis crap'
+  header = head.pack(SYN,ACK,seq_num,ack_num)
+  s.sendto(header,(receiver_host_ip, receiver_port)) #sends SYN
+  print 'SYN packet sent'
+  message, client = s.recvfrom(1024) #reads reply
+  print 'SYN+ACK received'
+  header = head.unpack(message)
+  print header
+  if(header[0] == True and header[1] == True):
+    SYN = False
+    ACK = True
+    seq_num = header[2]+1
+    ack_num = header[3]
+    header = head.pack(SYN, ACK, seq_num, ack_num)
+    s.sendto(header,(receiver_host_ip, receiver_port)) #sends ACK
+    print 'ACK packet sent'
+#handshakes
 except socket.error:
-    print 'Could not connect to server, terminating program.\n'
+  print 'Could not connect to server, terminating program.\n'
+    
+    
+
+    
