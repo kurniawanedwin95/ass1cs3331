5d7ad46030c1283771167b15536db2a1cab50671
kurniawanedwin95 <kurniawanedwin95@gmail.com>
2016-09-12 13:21:53 +1000

    removed struct, using pickle method now

--- receiver.py
+++ receiver.py
@@ -2,11 +2,8 @@
 
 #socket implementation
 import socket
-import struct
+import pickle
 import sys
-
-
-head = struct.Struct('? ? I I')
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 host = '' #local
@@ -17,18 +14,17 @@ print 'server is waiting for UDP connection'
 while 1:
   message, client = s.recvfrom(1024) #buffer size 1kb
   #connection received
-  header = head.unpack(message)
-  print header
+  message = pickle.loads(message)
+  print message
   #might need an if statement here
-  if(header[0] == True and header[1] == False):
-    SYN = True
-    ACK = True
-    seq_num = header[2]+1
-    ack_num = 1
-    header = head.pack(SYN, ACK, seq_num, ack_num)
-    s.sendto(header, client)
+  if(message['SYN'] == True and message['ACK'] == False):
+    value = {'SYN':True,'ACK':True,'seq_num':message['seq_num']+1,'ack_num':1}
+    message = pickle.dumps(value)
+    s.sendto(message, client)
     print 'SYN+ACK packet sent'
+    
   message, client = s.recvfrom(1024)
-  header = head.unpack(message)
-  if(header[0] == False and header[1] == True):
+  message = pickle.loads(message)
+  if(message['SYN'] == False and message['ACK'] == True):
     print 'ACK Received'
+    exit
--- sender.py
+++ sender.py
@@ -1,7 +1,7 @@
 #!usr/bin/python
 
 import socket
-import struct
+import pickle
 import sys
 import time
 
@@ -14,32 +14,27 @@ receiver_port = int(sys.argv[2])
 # timeout = sys.arg[5]
 # pdrop = sys.arg[6]
 # seed = sys.arg[7]
-head = struct.Struct('? ? I I')
+cwnd = 1
+# ssthresh = MSS #initial ssthresh value
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 try:
   #first we will simulate a TCP header and make it available in all our UDP packets
-  SYN = True
-  ACK = False
-  seq_num = 1#some randomly generated value
-  ack_num = 0
-  message = 'testin dis crap'
-  header = head.pack(SYN,ACK,seq_num,ack_num)
-  s.sendto(header,(receiver_host_ip, receiver_port)) #sends SYN
+  value = {'SYN':True, 'ACK':False, 'seq_num':1, 'ack_num':0, 'data':''} #seq shud be RNG
+  message = pickle.dumps(value)
+  s.sendto(message,(receiver_host_ip, receiver_port)) #sends SYN
   print 'SYN packet sent'
-  message, client = s.recvfrom(1024) #reads reply
+  
+  message, client = s.recvfrom(1024) #reads SYN+ACK
+  message = pickle.loads(message)
   print 'SYN+ACK received'
-  header = head.unpack(message)
-  print header
-  if(header[0] == True and header[1] == True):
-    SYN = False
-    ACK = True
-    seq_num = header[2]+1
-    ack_num = header[3]
-    header = head.pack(SYN, ACK, seq_num, ack_num)
-    s.sendto(header,(receiver_host_ip, receiver_port)) #sends ACK
+  print message
+  if(message['SYN'] == True and message['ACK'] == True):
+    value = {'SYN':False, 'ACK':True, 'seq_num':message['seq_num']+1, 'ack_num':message['ack_num']}
+    message = pickle.dumps(value)
+    s.sendto(message,(receiver_host_ip, receiver_port)) #sends ACK
     print 'ACK packet sent'
-#handshakes
+
 except socket.error:
   print 'Could not connect to server, terminating program.\n'
     
